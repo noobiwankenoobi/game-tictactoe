ee8fa9ea0e0ea62a5375bd076d2f1fc11b5bd085
noobiwankenoobi <ross.hazelett@gmail.com>
2019-12-26 00:03:05 -0500

    more

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const gameState = {
 
 }
 
+const winConditions = [
+  [1,2,3],
+  [1,5,9],
+  [1,4,7,],
+  [4,5,6],
+  [7,8,9],
+  [2,5,8],
+  [3,6,9],
+  [3,5,7]
+];
+
 const checkForVictory = () => {
   console.log('checking for Victory')
 }
