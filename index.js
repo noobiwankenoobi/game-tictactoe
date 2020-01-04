24464985053eae80ff3e36f767c8cd3852325afa
noobiwankenoobi <ross.hazelett@gmail.com>
2020-01-04 10:49:22 -0500

    tried getting checkVictory loop to work but it's running the win condition on every loop. It needs to check if each loop output matches the previous, and I haven't figured out how to do that yet. Trying a nested loop next

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const gameState = {
   playerOneSymbol: 'X',
   playerTwoSymbol: 'O',
   currentBoardArray: [,,,,,,,,,],
-  activeGame: true
-
+  activeGame: true,
+  gameOver: false,
+  gameWinner: "",
 }
 
 const winConditions = [
@@ -20,10 +21,44 @@ const winConditions = [
 ];
 
 const checkForVictory = () => {
-  console.log('checking for Victory')
+
+  console.log('Running: Checking for Victory')
+
+  let testArray = winConditions[0]
+  let checkIfXWins = 
+  console.log("first win condition=", winConditions[0])
+
+  for (let i = 0; i < testArray.length; i++) {
+    console.log("test log =", gameState.currentBoardArray[testArray[i]])
+    if (gameState.currentBoardArray[testArray[i]] === "x") {
+      match = true
+    }
+
+  for (let i = 0; i < testArray.length; i++) {
+    if (gameState.currentBoardArray[testArray[i]] !== "x") {
+      match = false;
+    }
+    
+  }
+  
+}
+if (match === true) {
+  console.log ("x wins")
+}
 }
 
+
+
+
+
+// if (cellsMatchX = true) {
+//   console.log("Player X Wins!")
+//
+
+
+
 const addToCell = (cellID, letter) => {
+  // puts the correct symbol onto the playing board
     $('#' + cellID).html(letter); 
     console.log(cellID)
     // putting symbols into correct position on currentBoardArray
@@ -48,13 +83,17 @@ const addToCell = (cellID, letter) => {
     } else if (cellID === "cell-nine") {
       arrayIndex = 8;
     }
+    // after assigning the index, this puts the letter into that index position
     gameState.currentBoardArray[arrayIndex] = letter;
+    // run the victory check after assigning to array
+    checkForVictory()
+
+    // console logs
     console.log("arrayIndex= ", arrayIndex);
     console.log("BOARD= ", gameState.currentBoardArray)
-    
-
 } 
 
+// checks to make sure cell is empty before putting current turn symbol and changing turn
 const playerMove = () => {
   if (gameState.activeGame && $('#' + event.target.id).html() === '') {
     if (gameState.currentTurn === 'playerOne') {
@@ -66,7 +105,7 @@ const playerMove = () => {
     } else {
       console.error('player state unknown')
     }
-    checkForVictory()
+    
   }
 }
 
