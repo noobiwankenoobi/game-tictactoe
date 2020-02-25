4d1a1a9856c6381be7d30b5dcc2159faa938b2bc
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-02-25 17:15:32 -0500

    cleaned up comments and logs

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// GAME OBJECT // GAME OBJECT
-
 const startingGameState = {
   currentTurn: 'playerOne',
   playerOneSymbol: 'X',
@@ -35,12 +33,7 @@ const winConditions = [
   [2,4,6]
 ];
 
-// NEW GAME FUNCTION
-
 const newGame = () => {
-  // this will clean up a bunch of the lower functions that are resetting things themselves
-  // reset the board, reset the gameState, reset everything on screen that needs resetting
-  console.log("RESET BOARD IS RUNNING!")
   $('.cell').html("");
   gameState.gameOver = false;
   gameState.activeGame = true;
@@ -51,17 +44,9 @@ const newGame = () => {
   for (let i = 0; i < gameState.currentBoardArray.length; i++) {
     gameState.currentBoardArray[i] = "";
   }
-
-  console.log("currentBoardArray=", gameState.currentBoardArray)
 }
 
-// FUNCTION THAT RUNS WHEN WINNER IS DETERMINED AND NEEDS ANNOUNCING
-// FUNCTION THAT RUNS WHEN WINNER IS DETERMINED AND NEEDS ANNOUNCING
-
 const announceWinner = (currentGameWinner) => {
-  // Create a modal in future that announces the winner with a "Player again" button that runs a newGame function
-  console.log("ANOUNCE WINNER IS RUNNING")
-  console.log("currentGameWinner", currentGameWinner)
   if (currentGameWinner === "X") {
     console.log("X conditions running")
     $('.announce-winner-div').html("Player One Wins!");
@@ -69,9 +54,6 @@ const announceWinner = (currentGameWinner) => {
     $('.announce-winner-div').html("Player Two Wins!");
   }
 }
-
-// UPDATE PLAYER TURN ON SCREEN
-// UPDATE PLAYER TURN ON SCREEN
 
 const updatePlayerTurnOnScreen = () => {
   if (gameState.currentTurn === "playerOne" && gameState.activeGame === true) {
@@ -82,9 +64,6 @@ const updatePlayerTurnOnScreen = () => {
     $('.current-player-div').html("")
   }
 }
-
-// RESET BOARD ON SCREEN AND GAME STATE
-// RESET BOARD ON SCREEN AND GAME STATE
 
 const resetBoard = () => {
   console.log("RESET BOARD IS RUNNING!")
@@ -98,12 +77,7 @@ const resetBoard = () => {
   for (let i = 0; i < gameState.currentBoardArray.length; i++) {
     gameState.currentBoardArray[i] = "";
   }
-
-  console.log("currentBoardArray=", gameState.currentBoardArray)
 }
-
-// UPDATE WINS TABLE ON THE SCREEN
-// UPDATE WINS TABLE ON THE SCREEN
 
 const updateWinsTable = (currentGameWinner) => {
   if (currentGameWinner === "X") {
@@ -113,13 +87,8 @@ const updateWinsTable = (currentGameWinner) => {
   }
 }
 
-// CHECK WHO WINS AFTER VICTORY IS DETERMINED
-// CHECK WHO WINS AFTER VICTORY IS DETERMINED
-
-
 const checkWhoWins = (winTestArray) => {
-  // console.log("checkXWins is RUNNING!");
-  // console.log("arr1=", arr1);
+
   let currentGameWinner ="";
 
   if (winTestArray[0] === 'X' && winTestArray[1] === 'X' && winTestArray[2] === 'X') {
@@ -129,9 +98,6 @@ const checkWhoWins = (winTestArray) => {
     gameState.activeGame = false;
     
     currentGameWinner = "X";
-
-    console.log("X WINS!")
-    console.log(gameState)
 
     updateWinsTable(currentGameWinner)
     announceWinner(currentGameWinner)
@@ -144,18 +110,12 @@ const checkWhoWins = (winTestArray) => {
     
     currentGameWinner = "O";
 
-    console.log("O WINS!")
-    console.log(gameState)
-
     updateWinsTable(currentGameWinner)
     announceWinner(currentGameWinner)
   } else {
-    // console.log("KEEP PLAYING!")
+    console.error("error in checkWhoWins")
   }
 }
- 
-// FIRST CHECK FOR A VICTORY
-// FIRST CHECK FOR A VICTORY
 
   const checkForVictory = () => {
 
@@ -164,22 +124,16 @@ const checkWhoWins = (winTestArray) => {
       let winTestArray = [,,,];
 
       if (gameState.activeGame === true) {
-      console.log("NEXT ITERATION-----------")
 
         for (let j = 0; j < 3; j++) {
           
-          // THIS IF STATEMENT (MAYBE) NEEDS FIXING!!
           if (winTestArray[0] !== "" && winTestArray[1] !== "" && winTestArray[2] !== "") {
       
             let indexes = winConditions[i][j];
             let boardValues = gameState.currentBoardArray[indexes]
             
-            // testArray IS BEING ASSIGNED VALUES IN CERTAIN INDEXES BY THE CURRENT BOARD ARRAY
             winTestArray[j] = boardValues;
-            console.log("indexes=", indexes)
-            console.log("winTestArray=", winTestArray);
             
-            // runs checkWins and sends the iteration of testArray every loop to be checked by checkWhoWins
             checkWhoWins(winTestArray)
             
           }
@@ -188,15 +142,9 @@ const checkWhoWins = (winTestArray) => {
     }
   } 
 
-// ADDS SYMBOL TO GAMEBOARD IN DOM // ADDS SYMBOL TO GAMEBOARD IN DOM
-// ADDS SYMBOL TO GAMEBOARD IN DOM // ADDS SYMBOL TO GAMEBOARD IN DOM
-// ADDS SYMBOL TO GAMEBOARD IN DOM // ADDS SYMBOL TO GAMEBOARD IN DOM
-
 const addToCell = (cellID, letter) => {
-  // puts the correct symbol onto the playing board
     $('#' + cellID).html(letter); 
 
-    // putting symbols into correct position on currentBoardArray
     let arrayIndex = ""
 
     if (cellID === "cell-one") {
@@ -218,20 +166,13 @@ const addToCell = (cellID, letter) => {
     } else if (cellID === "cell-nine") {
       arrayIndex = 8;
     }
-    // after assigning the index, this puts the letter into that index position
+
     gameState.currentBoardArray[arrayIndex] = letter;
-    // run the victory check after assigning to array
+
     checkForVictory()
    
 } 
 
-
-//  PLAYER MOVE FUNCTION // PLAYER MOVE FUNCTION
-//  PLAYER MOVE FUNCTION // PLAYER MOVE FUNCTION
-//  PLAYER MOVE FUNCTION // PLAYER MOVE FUNCTION
-
-//  Makes sure that thee is an activeGame and that the target cell is empty
-//  Adds correct symbol based on currentTurn, then switches the currentTurn to the other player
 const playerMove = () => {
   if (gameState.activeGame && $('#' + event.target.id).html() === '') {
 
@@ -250,25 +191,16 @@ const playerMove = () => {
       updatePlayerTurnOnScreen()
 
     } else {
-
       console.error('player state unknown')
     }
     
   }
 }
 
-// CLICK HANDLERS // CLICK HANDLERS
-// CLICK HANDLERS // CLICK HANDLERS
-// CLICK HANDLERS // CLICK HANDLERS
-
 const addHandlers = () => {
   $('.cell').on('click', playerMove)
   $('#board-reset-button').on('click', resetBoard)
 };
-
-// ON PAGE LOAD // ON PAGE LOAD
-// ON PAGE LOAD // ON PAGE LOAD
-// ON PAGE LOAD // ON PAGE LOAD
 
 $(() => {
   addHandlers()
